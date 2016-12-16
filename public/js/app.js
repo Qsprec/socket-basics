a9ba59f9c2fdeb3c9d0f9c2dc268277adaedc7e7
Ahmet Ozan Ekici <aozan.ekici@gmail.com>
2016-12-16 23:11:52 +0300

    show timestamp on ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,22 @@
 var socket = io();
 
-socket.on('connect', function () {
+socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
 });
 
-socket.on('message', function (message) {
+socket.on('message', function(message) {
 	console.log("New message");
 	console.log(message.text);
-
-	jQuery('.messages').append('<p>'+message.text+'</p>');
+	var momentTimestamp = moment.utc(message.timestamp);
+	var timeString = momentTimestamp.local().format('h:mm a');
+	jQuery('.messages').append('<p><strong>' + timeString + "</strong>: " + message.text + '</p>');
 });
 
 
 // Handles submitting new message
 var $form = jQuery('#message-form');
 
-$form.on('submit', function (event) {
+$form.on('submit', function(event) {
 	event.preventDefault();
 
 	var $message = $form.find('input[name=message]');
